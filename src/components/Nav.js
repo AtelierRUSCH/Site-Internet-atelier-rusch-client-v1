cd0d7de9b47611435b88d2ffb1d67b2d79393ca8
MarieMalarme <malarmemarie@gmail.com>
2018-11-17 20:16:15 +0100

    fixing git remote

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -18,30 +18,8 @@ const Nav = ({onPageChange}) =>
 
     <div className="NavBarLinksContainer">
       <Link onClick={() => window.scrollTo(0, 0)} className={window.location.pathname === '/atelier' ? 'NavBarLink active' : 'NavBarLink'} to='/atelier'>Atelier</Link>
-<<<<<<< HEAD
       <Link onClick={() => { window.scrollTo(0, 0) }} className={window.location.pathname === '/projets' ? 'NavBarLink active' : 'NavBarLink'} to='/projets'>Projets</Link>
       <Link onClick={() => { window.scrollTo(0, 0) }} className={window.location.pathname === '/lab' ? 'NavBarLink active' : 'NavBarLink'} to='/lab'>LabRusch</Link>
-=======
-      <Link onClick={() => {
-        store.dispatch(loadArticles(this.state.articles))
-        window.scrollTo(0, 0)
-<<<<<<< HEAD
-      }} className={window.location.pathname === '/projets' ? 'NavBarLink active' : 'NavBarLink'} to='/projets'>Projets</Link>
-=======
-        window.history.go(0)
-        console.log('okokok')
-      }} className={window.location.pathname === '/projets' ? 'NavBarLink active' : 'NavBarLink'} to='/projets'>Projects</Link>
->>>>>>> 0f39d05068f610f418850753422352b0ef756cc8
-      <Link onClick={() => {
-        store.dispatch(loadArticles(this.state.articles))
-        window.scrollTo(0, 0)
-<<<<<<< HEAD
-=======
-        window.history.go(0)
-        console.log('okokok')
->>>>>>> 0f39d05068f610f418850753422352b0ef756cc8
-      }} className={window.location.pathname === '/lab' ? 'NavBarLink active' : 'NavBarLink'} to='/lab'>LabRusch</Link>
->>>>>>> a78513ec55215005a88b4f8cb756a2b235e0f106
       <Link onClick={() => window.scrollTo(0, 0)} className={window.location.pathname === '/contact' ? 'NavBarLink active' : 'NavBarLink'} to='/contact'>Contact</Link>
     </div>
 
