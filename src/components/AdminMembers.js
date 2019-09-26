856d139ae5bae09e843ada008f8e09b1d1b870d4
Marie Malarme <malarmemarie@gmail.com>
2019-09-26 23:15:09 +0100

    Lint & prettier.

--- src/components/AdminMembers.js
+++ src/components/AdminMembers.js
@@ -6,71 +6,77 @@ import IoEdit from 'react-icons/lib/io/edit'
 import MdAdd from 'react-icons/lib/md/add-circle-outline'
 
 const AdminMembers = ({ members, thanks }) => {
-  const displayMembers = members
-    .map(el =>
-      <div className='AdminCardHalfWidth'>
-        <div className='currentText'> {el.name} </div>
-        <div className='EditButtonsContainer'>
-          <Link to={String(el.id)}>
-            <button className="ReactIcon">
-              <IoEdit/>
-            </button>
-          </Link>
-          <button className="ReactIcon" onClick={() => {
-            if (window.confirm('la suppression est irréversible. Pas de regrets?')) {
-              api.deleteMember(el.id)
-                .then(() => window.location.reload())
-            } else {
-
-            }
-          }}>
-            <IoAndroidDelete/>
+  const displayMembers = members.map(el => (
+    <div className="AdminCardHalfWidth">
+      <div className="currentText"> {el.name} </div>
+      <div className="EditButtonsContainer">
+        <Link to={String(el.id)}>
+          <button className="ReactIcon">
+            <IoEdit />
           </button>
-        </div>
-      </div>
-    )
-
-    const displayThanks = thanks
-    .map(el =>
-      <div className='AdminCardHalfWidth'>
-        <div className='currentText'> {el.name} </div>
-        <div className='EditButtonsContainer'>
-          <Link to={`thanks/${String(el.id)}`}>
-            <button className="ReactIcon">
-              <IoEdit/>
-            </button>
-          </Link>
-          <button className="ReactIcon" onClick={() => {
-            if (window.confirm('la suppression est irréversible. Pas de regrets?')) {
-              api.deleteThank(el.id)
-                .then(() => window.location.reload())
+        </Link>
+        <button
+          className="ReactIcon"
+          onClick={() => {
+            if (
+              window.confirm('la suppression est irréversible. Pas de regrets?')
+            ) {
+              api.deleteMember(el.id).then(() => window.location.reload())
             } else {
-
             }
-          }}>
-            <IoAndroidDelete/>
-          </button>
-        </div>
+          }}
+        >
+          <IoAndroidDelete />
+        </button>
       </div>
-    )
+    </div>
+  ))
+
+  const displayThanks = thanks.map(el => (
+    <div className="AdminCardHalfWidth">
+      <div className="currentText"> {el.name} </div>
+      <div className="EditButtonsContainer">
+        <Link to={`thanks/${String(el.id)}`}>
+          <button className="ReactIcon">
+            <IoEdit />
+          </button>
+        </Link>
+        <button
+          className="ReactIcon"
+          onClick={() => {
+            if (
+              window.confirm('la suppression est irréversible. Pas de regrets?')
+            ) {
+              api.deleteThank(el.id).then(() => window.location.reload())
+            } else {
+            }
+          }}
+        >
+          <IoAndroidDelete />
+        </button>
+      </div>
+    </div>
+  ))
 
   return (
-    <div className='GlobalContainer'>
-      <Link to='new' onClick={() => window.scrollTo(0, 0)}>
-        <div className='ButtonCreateElement'><MdAdd className='ReactIconAdd' />Créer un nouveau membre</div>
+    <div className="GlobalContainer">
+      <Link to="new" onClick={() => window.scrollTo(0, 0)}>
+        <div className="ButtonCreateElement">
+          <MdAdd className="ReactIconAdd" />
+          Créer un nouveau membre
+        </div>
       </Link>
-      <div className='AdminTitles yellow'>Membres de l'équipe :</div>
-      <div className='AdminCardHalfWidthContainer'>
-        {displayMembers}
-      </div>
+      <div className="AdminTitles yellow">Membres de l'équipe :</div>
+      <div className="AdminCardHalfWidthContainer">{displayMembers}</div>
 
-      <Link to='thanks/new' onClick={() => window.scrollTo(0, 0)}>
-        <div style={{ marginTop: '6rem' }} className='ButtonCreateElement'><MdAdd className='ReactIconAdd' />Créer un nouveau remerciement</div>
+      <Link to="thanks/new" onClick={() => window.scrollTo(0, 0)}>
+        <div style={{ marginTop: '6rem' }} className="ButtonCreateElement">
+          <MdAdd className="ReactIconAdd" />
+          Créer un nouveau remerciement
+        </div>
       </Link>
-      <div className='AdminTitles yellow'>Remerciements :</div>
-      <div className='AdminCardHalfWidthContainer'>
-        {displayThanks}
-      </div>
+      <div className="AdminTitles yellow">Remerciements :</div>
+      <div className="AdminCardHalfWidthContainer">{displayThanks}</div>
     </div>
   )
 }
--- src/components/ArticleThumbnail.js
+++ src/components/ArticleThumbnail.js
@@ -8,34 +8,55 @@ const ArticleThumbnail = ({ article, index }) => {
   const scrollTop = () => {
     const numberRegex = /\d/g
     const isArticlePage = numberRegex.test(window.location.pathname)
-    if ((isArticlePage) || (window.location.pathname === '/')) {
+    if (isArticlePage || window.location.pathname === '/') {
       document.getElementById('scrollToTop').scrollIntoView()
     }
   }
 
   return (
-    <Link
-      to={`/${articleSection}/${articleId}`}
-      onClick={() => scrollTop()}>
-      <div className='ArticleThumbnailClassic'
-        style={article.hasImage === '1' ? { zIndex: index, background: `center / cover no-repeat url("${article.headerImage}"), rgba(0, 0, 0, 0.5)` } : { zIndex: index }}>
-        <div className={(article.hasImage === '1' && window.location.pathname !== '/') ? 'ArticleThumbnailFilterBlack ArticleThumbnailHasImage' : 'ArticleThumbnailClassic'}>
-          <h6>
-            {article.tags.join(' - ')}
-          </h6>
-          <h5>
-            {article.title}
-          </h5>
-            {article.section === 'projets'
-              ? <React.Fragment>
-                  <h6 style={{ marginLeft: '2rem', marginRight: '2rem', lineHeight: '1.2rem' }}>
-                    {article.client}
-                    <div style={{ height: '0.4rem' }} />
-                    {article.place}
-                  </h6>
-                </React.Fragment>
-              : <h6>{`${article.date.slice(-2)}.${article.date.slice(5, 7)}.${article.date.slice(0, 4)}`}</h6>
-            }
+    <Link to={`/${articleSection}/${articleId}`} onClick={() => scrollTop()}>
+      <div
+        className="ArticleThumbnailClassic"
+        style={
+          article.hasImage === '1'
+            ? {
+                zIndex: index,
+                background: `center / cover no-repeat url("${
+                  article.headerImage
+                }"), rgba(0, 0, 0, 0.5)`
+              }
+            : { zIndex: index }
+        }
+      >
+        <div
+          className={
+            article.hasImage === '1' && window.location.pathname !== '/'
+              ? 'ArticleThumbnailFilterBlack ArticleThumbnailHasImage'
+              : 'ArticleThumbnailClassic'
+          }
+        >
+          <h6>{article.tags.join(' - ')}</h6>
+          <h5>{article.title}</h5>
+          {article.section === 'projets' ? (
+            <React.Fragment>
+              <h6
+                style={{
+                  marginLeft: '2rem',
+                  marginRight: '2rem',
+                  lineHeight: '1.2rem'
+                }}
+              >
+                {article.client}
+                <div style={{ height: '0.4rem' }} />
+                {article.place}
+              </h6>
+            </React.Fragment>
+          ) : (
+            <h6>{`${article.date.slice(-2)}.${article.date.slice(
+              5,
+              7
+            )}.${article.date.slice(0, 4)}`}</h6>
+          )}
         </div>
       </div>
     </Link>
