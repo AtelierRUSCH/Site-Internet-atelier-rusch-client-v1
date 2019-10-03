16ec838d4cb618d29a963e01b74b98b34bc49bb1
Marie Malarme <malarmemarie@gmail.com>
2019-10-03 22:38:26 +0100

    Replaced the div tag of the navbar by nav.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,30 +3,69 @@ import { Link } from '@reach/router'
 import './css/Nav.css'
 import Logo from './img/logo-rusch-noir.png'
 
-const Nav = ({onPageChange}) =>
-  <div className="NavBar">
-
-    <div className='Logo'>
-      <Link onClick={() => window.scrollTo(0, 0)} to='/'>
-        <button className='HomeLink' value='Home'>
-          <img style={{ width: '75px' }} src={Logo} alt='logo-rusch-noir' />
+const Nav = ({ onPageChange }) => (
+  <nav className="NavBar">
+    <div className="Logo">
+      <Link onClick={() => window.scrollTo(0, 0)} to="/">
+        <button className="HomeLink" value="Home">
+          <img style={{ width: '75px' }} src={Logo} alt="logo-rusch-noir" />
         </button>
       </Link>
     </div>
 
     <div className="NavBarLinksContainer">
-      <Link onClick={() => window.scrollTo(0, 0)} className={window.location.pathname === '/atelier' ? 'NavBarLink active' : 'NavBarLink'} to='/atelier'>Atelier</Link>
-      <Link onClick={(props) => {
-        window.scrollTo(0, 0)
-        props.history.go(0)
-      }} className={window.location.pathname === '/projets' ? 'NavBarLink active' : 'NavBarLink'} to='/projets'>Projets</Link>
-      <Link onClick={(props) => {
-        window.scrollTo(0, 0)
-        props.history.go(0)
-      }} className={window.location.pathname === '/lab' ? 'NavBarLink active' : 'NavBarLink'} to='/lab'>LabRusch</Link>
-      <Link onClick={() => window.scrollTo(0, 0)} className={window.location.pathname === '/contact' ? 'NavBarLink active' : 'NavBarLink'} to='/contact'>Contact</Link>
+      <Link
+        onClick={() => window.scrollTo(0, 0)}
+        className={
+          window.location.pathname === '/atelier'
+            ? 'NavBarLink active'
+            : 'NavBarLink'
+        }
+        to="/atelier"
+      >
+        Atelier
+      </Link>
+      <Link
+        onClick={props => {
+          window.scrollTo(0, 0)
+          props.history.go(0)
+        }}
+        className={
+          window.location.pathname === '/projets'
+            ? 'NavBarLink active'
+            : 'NavBarLink'
+        }
+        to="/projets"
+      >
+        Projets
+      </Link>
+      <Link
+        onClick={props => {
+          window.scrollTo(0, 0)
+          props.history.go(0)
+        }}
+        className={
+          window.location.pathname === '/lab'
+            ? 'NavBarLink active'
+            : 'NavBarLink'
+        }
+        to="/lab"
+      >
+        LabRusch
+      </Link>
+      <Link
+        onClick={() => window.scrollTo(0, 0)}
+        className={
+          window.location.pathname === '/contact'
+            ? 'NavBarLink active'
+            : 'NavBarLink'
+        }
+        to="/contact"
+      >
+        Contact
+      </Link>
     </div>
-
-  </div>
+  </nav>
+)
 
 export default Nav
